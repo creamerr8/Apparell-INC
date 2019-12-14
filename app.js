6c0b2b0408435059014fe4eaa07acd5e3cd28014
Ryan <creamerr8@gmail.com>
2019-12-14 15:39:29 -0800

    edited js prompts

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-var hournow = prompt('what time is it? 0-23');
-console.log('prompt question:', hournow);
+var apparel = prompt(' Do you like apparel? yes or no ');
+console.log('prompt question:', apparel);
 
-var greeting;
+var apparel;
 
-if (hournow > 18 && hournow < 24){
-    greeting = 'good evening, everyone!'
+if (apparel === 'yes'){
+    greeting = 'Awesome, you came to the right place';
 }
-
-else if (hournow > 12 && hournow <24){
-    greeting = 'good afternoon, everyone'
-}
-
-else if (hournow >= 0 && hournow < 24){
-    greeting = 'good morning'
-} else {
-    greeting = 'wait you didnt give me a proper response'
-}
-
+ else if ( apparel === 'no'){
+     greeting = 'Go Away!!!';
+ } else{
+     greeting = 'That was an incorrect response'
+ }
 
 console.log('our response:', greeting);
 
-document.write(greeting);
-document.write( ' i come after the hour from my greeting ' )
-document.write( ' i come after the statement of i come after ' )
+ document.write(greeting);
